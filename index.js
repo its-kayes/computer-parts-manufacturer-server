8c2247a62f56181f791c7e18189d393825b12d43
its-kayes <kayes.ek8@gmail.com>
2022-05-22 16:03:27 +0600

    Server setup and load data from db

---
+++ index.js
@@ -0,0 +1,50 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+let app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yffmf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        let partsCollection = client.db("partsdb").collection("parts");
+
+        app.get('/parts', async(req, res)=> {
+            let query = {}
+            let data = await partsCollection.find(query).toArray();
+            res.send(data);
+        })
+    }
+
+    finally{
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Parts Manufacturer Is On Fire ');
+});
+
+app.listen(port, () => {
+    console.log('Run Server to port', port);
+})
